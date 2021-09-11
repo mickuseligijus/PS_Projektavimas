df9c1dd708b16a9c2724bd94667e612dd15595b8
mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>
2021-09-11 15:01:18 +0300

    few tests for phone number validation

--- Test/lt/vu/mif/validators/TestValidators.java
+++ Test/lt/vu/mif/validators/TestValidators.java
@@ -1,9 +1,12 @@
 package lt.vu.mif.validators;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestValidators {
     private PhoneValidator phoneValidator;
@@ -17,12 +20,58 @@ public class TestValidators {
         passwordChecker = new PasswordChecker();
     }
     @Test
-    public void TestEmptyPhoneNumber(){
-        String number = "    ";
-        assertFalse(phoneValidator.isPhoneNumberValid(number));
+    public void TestNoSpaceBarsInPhoneNumber(){
+        String number = "+370 343 00 000";
+        Assertions.assertFalse(phoneValidator.isPhoneNumberValid(number));
     }
     @Test
+    public void TestNoOtherSymbolsInPhoneNumber(){
+        String number = "(86) 21 380 62";
+        Assertions.assertFalse(phoneValidator.isPhoneNumberValid(number));
+    }
+    @Test
+    public void TestPhoneNumberIsShorterByOne(){
+        String number = "+3706213906";
+        Assertions.assertFalse(phoneValidator.isPhoneNumberValid(number));
+    }
+    @Test
+    public void TestPhoneNumberIsLongerByTwo(){
+        String number = "+3706217806238";
+        Assertions.assertFalse(phoneValidator.isPhoneNumberValid(number));    }
+    @Test
+    public void TestEmptyPhoneNumber(){
+        String number = "    ";
+        Assertions.assertFalse(phoneValidator.isPhoneNumberValid(number));    }
+    @Test
+    public void TestNullNumber(){
+        Assertions.assertFalse(phoneValidator.isPhoneNumberValid(null));
+    }
+    @Test
+    //
     public void TestPhoneNumberStartsWithLetter(){
-        
+        String number = "c62138079";
+        Assertions.assertFalse(phoneValidator.isPhoneNumberValid(number));
+    }
+    @Test
+    public void TestPhoneNumberStartsWithIncorrectSymbol(){
+        String number = "-37062157469";
+        Assertions.assertFalse(phoneValidator.isPhoneNumberValid(number));
+    }
+    @Test
+    public void TestPhoneNumberReplacesPrefix(){
+        String number = "869721384";
+        Assertions.assertEquals("+37069721384", phoneValidator.replacePhoneNumberPrefix(number));
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"+37062138062", "862138062","+37062891542","862891542"})
+    public void TestIsNumberValidCorrect(String input){
+        assertTrue(phoneValidator.isPhoneNumberValid(input));
+    }
+
+    @Test
+    public void TestIsAnotherStateNumberIsValid(){
+        String number = "+38760123456";
+        Assertions.assertTrue(phoneValidator.isPhoneNumberValidByState(number,"+387","60",8));
+
     }
 }
--- src/lt/vu/mif/validators/PhoneValidator.java
+++ src/lt/vu/mif/validators/PhoneValidator.java
@@ -1,7 +1,15 @@
 package lt.vu.mif.validators;
 
 public class PhoneValidator {
+    public boolean isPhoneNumberValidByState(String number,String internationalCallingCode, String mobilePrefix, int sizeOfNSN) {
+        return false;
+    }
+
     public boolean isPhoneNumberValid(String number) {
-        return true;
+        return false;
+    }
+
+    public String replacePhoneNumberPrefix(String number) {
+        return "";
     }
 }
