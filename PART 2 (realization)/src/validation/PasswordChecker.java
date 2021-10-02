1f48e1d674b3bba399ed41c8b7c61c1ad0a4cbab
mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>
2021-10-02 11:36:24 +0300

    upper case PW

--- PART 2 (realization)/src/validation/PasswordChecker.java
+++ PART 2 (realization)/src/validation/PasswordChecker.java
@@ -1,12 +1,18 @@
 package validation;
 
 public class PasswordChecker {
+    
     public static boolean checkLength(String password, int minLength, int maxLength) {
-
         return password.length()>= minLength && password.length()<=maxLength && password != null;
     }
 
     public static boolean checkUppercase(String password) {
+
+        for (int i=0; i<password.length(); i++){
+           if(Character.isUpperCase(password.charAt(i))) {
+               return true;
+           }
+        }
         return false;
     }
 
