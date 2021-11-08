8945394c61eac9b5f368fc5be8a6521a408fd495
mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>
2021-11-08 13:51:13 +0200

    created spring boot project

---
+++ PART 3 (usage)/validateapp/src/main/java/com/mif/vu/validateapp/ValidateappApplication.java
@@ -0,0 +1,13 @@
+package com.mif.vu.validateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidateappApplication.class, args);
+	}
+
+}
---
+++ PART 3 (usage)/validateapp/src/test/java/com/mif/vu/validateapp/ValidateappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mif.vu.validateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidateappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
