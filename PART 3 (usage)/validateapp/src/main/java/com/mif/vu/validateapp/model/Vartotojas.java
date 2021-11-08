a9e929c89eb4fa7ab2d28d54b81266fce7e587c8
mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>
2021-11-08 18:18:08 +0200

    connecting db and creating Vartotojas entity

---
+++ PART 3 (usage)/validateapp/src/main/java/com/mif/vu/validateapp/model/Vartotojas.java
@@ -0,0 +1,67 @@
+package com.mif.vu.validateapp.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Vartotojas {
+	
+	private String vardas;
+	private String pavarde;
+	private String telefonoNumeris;
+	private String email;
+	private String adresas;
+	private String slaptazodis;
+	
+	public Vartotojas() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Vartotojas(String vardas, String pavarde, String telefonoNumeris, String email, String adresas,
+			String slaptazodis) {
+		super();
+		this.vardas = vardas;
+		this.pavarde = pavarde;
+		this.telefonoNumeris = telefonoNumeris;
+		this.email = email;
+		this.adresas = adresas;
+		this.slaptazodis = slaptazodis;
+	}
+	public String getVardas() {
+		return vardas;
+	}
+	public void setVardas(String vardas) {
+		this.vardas = vardas;
+	}
+	public String getPavarde() {
+		return pavarde;
+	}
+	public void setPavarde(String pavarde) {
+		this.pavarde = pavarde;
+	}
+	public String getTelefonoNumeris() {
+		return telefonoNumeris;
+	}
+	public void setTelefonoNumeris(String telefonoNumeris) {
+		this.telefonoNumeris = telefonoNumeris;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdresas() {
+		return adresas;
+	}
+	public void setAdresas(String adresas) {
+		this.adresas = adresas;
+	}
+	public String getSlaptazodis() {
+		return slaptazodis;
+	}
+	public void setSlaptazodis(String slaptazodis) {
+		this.slaptazodis = slaptazodis;
+	}
+	
+
+}
