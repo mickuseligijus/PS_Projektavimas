9ff3c0cfab54c61a48d87360b228999fe27be334
mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>
2021-11-08 18:51:29 +0200

    implementing service logic:

--- PART 3 (usage)/validateapp/src/main/java/com/mif/vu/validateapp/service/ValidationService.java
+++ PART 3 (usage)/validateapp/src/main/java/com/mif/vu/validateapp/service/ValidationService.java
@@ -1,5 +1,26 @@
 package com.mif.vu.validateapp.service;
 
+import org.springframework.stereotype.Service;
+
+import com.mif.vu.validateapp.library.Validator;
+
+@Service
 public class ValidationService {
 
+	private Validator validator;
+	public ValidationService() {
+		validator = new Validator();
+	}
+	
+	public boolean validatePassword(String passowrd) {
+		return validator.validatePassword(passowrd);
+	}
+	
+	public boolean validateEmail(String email) {
+		return validator.validatePassword(email);
+	}
+	
+	public boolean validatePhone(String phone) {
+		return validator.validatePhone(phone);
+	}
 }
